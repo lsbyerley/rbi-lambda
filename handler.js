f1d12ac9e61dec67b44e17d1a03d173c1727fd1f
lsbyerley <lsbyerley@gmail.com>
2019-11-27 12:56:24 -0500

    add lambda-api library

--- handler.js
+++ handler.js
@@ -1,162 +1,78 @@
-'use strict';
+'use strict'
 
-const axios = require('axios')
-const dayjs = require('dayjs')
-const _find = require('lodash/find')
-const _get = require('lodash/get')
+// Require and init API router module
+const app = require('lambda-api')({ version: 'v1.0', base: 'v1' })
+const cageSchedule = require('./lib/cageSchedule');
 
-const resourcesUrl = 'https://api.mindbodyonline.com/public/v6/site/resources'
-const tokenUrl = 'https://api.mindbodyonline.com/public/v6/usertoken/issue'
-const apptUrl = 'https://api.mindbodyonline.com/public/v6/appointment/staffappointments'
+//----------------------------------------------------------------------------//
+// Define Middleware
+//----------------------------------------------------------------------------//
 
-const AWS = require('aws-sdk')
-AWS.config.update({region: 'us-east-1'});
-const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-const getItemParams = {
-  TableName: 'MBOToken',
-  Key: {'ID': 1}
-};
+  // Add CORS Middleware
+  app.use((req,res,next) => {
 
-const usesb = false;
-const siteId = (usesb) ? process.env.MINDBODY_SITE_ID_SANDBOX : process.env.MINDBODY_SITE_ID;
-let accessToken = ''
+    // Add default CORS headers for every request
+    res.cors()
 
-const cageSchedule = async () => {
+    // Call next to continue processing
+    next()
+  })
 
-  try {
-    const dbItem = await docClient.get(getItemParams).promise()
-    const updatedAt = dayjs(dbItem.Item.UPDATED_AT)
-    const minutesOld = dayjs().diff(updatedAt, 'minute')
-    let usingFreshToken = false;
 
-    if (minutesOld > 60) { 
-      console.log('using fresh token')
-      accessToken = await getAccessToken();
-      usingFreshToken = true;
+  // Add Authorization Middleware
+  app.use((req,res,next) => {
 
-    } else {
-      console.log('using stored token')
-      accessToken = dbItem.Item.ACCESS_TOKEN
+    // Check for Authorization Bearer token
+    if (req.auth.type === 'Bearer') {
+      // Get the Bearer token value
+      let token = req.auth.value
+      // Set the token in the request scope
+      req.token = token
+      // Do some checking here to make sure it is valid (set an auth flag)
+      req.auth = true
     }
 
-    const resData = await axios.get(resourcesUrl, {
-      headers: {
-        'Api-Key': process.env.MINDBODY_API_KEY,
-        'SiteId': siteId,
-        'Authorization': accessToken
-      }
-    })
-    const apptData = await axios.get(apptUrl, {
-      params: {
-        startDate: '2019-07-31T00:00:00',
-        endDate: '2019-07-31T23:59:59'
-      },
-      headers: {
-        'Api-Key': process.env.MINDBODY_API_KEY,
-        'SiteId': siteId,
-        'Authorization': accessToken
-      }
-    })
+    // Call next to continue processing
+    next()
+  })
 
-    const resources = resData.data.Resources;
-    const appointments = apptData.data.Appointments;
+//----------------------------------------------------------------------------//
+// Build API routes
+//----------------------------------------------------------------------------//
 
-    return {
-      error: false,
-      token: {
-        minutesOld,
-        usingFreshToken
-      },
-      cageSchedule: buildCageSchedule(resources, appointments)
-    }
+  app.get('/cageSchedule', async (req,res) => {
 
-  } catch(err) {
-    console.error(err)
-    return {
-      error: true,
-      errorNote: err.message || 'no error message'
-    }
-  }
+    //let useSb = _get(req, 'query.useSandbox') || false;
+    let cageRes = await cageSchedule()
 
-}
-
-function buildCageSchedule(resources, appointments) {
-
-  let cageSchedules = [];
-  resources.forEach((r) => {
-
-    let rMap = {
-      id: r.ID,
-      name: r.Name,
-      appointments: []
-    };
-
-    appointments.forEach((a) => {
-      if (a.Resources.length > 0) {
-        if ( _find(a.Resources, { 'Id': r.Id }) ) {
-          const aStartTime = dayjs(a.StartDateTime).format("h:mm a");
-          const aEndTime = dayjs(a.EndDateTime).format("h:mm a");
-          rMap.appointments.push({
-            aStartTime,
-            aEndTime
-          })
-        }
-      }
-    })
-    cageSchedules.push(rMap);
+    // Send the response
+    res.status(200).json(cageRes)
 
   })
 
-  return cageSchedules;
+  // Default Options for CORS preflight
+  app.options('/*', (req,res) => {
+    res.status(200).json({})
+  })
 
-}
+  // Log routes to console for debugging
+  //app.routes(true)
 
-async function getAccessToken() {
-  const res = await axios.post(tokenUrl, 
-    {
-      Username: process.env.MINDBODY_USERNAME,
-      Password: process.env.MINDBODY_PASSWORD
-    }, 
-    {
-      headers: {
-        'Api-Key': process.env.MINDBODY_API_KEY,
-        'SiteId': siteId
-      }
-    }
-  );
-  
-  const updateItemParams = {
-    TableName: 'MBOToken',
-    Key: {'ID': 1},
-    UpdateExpression: 'set ACCESS_TOKEN = :at, UPDATED_AT = :ua',
-    ExpressionAttributeValues: {
-      ':at' : res.data.AccessToken,
-      ':ua' : dayjs().format('M/D/YYYY h:m a')
-    }
-  }
-  await docClient.update(updateItemParams).promise()
+//----------------------------------------------------------------------------//
+// Main router handler
+//----------------------------------------------------------------------------//
+module.exports.router = (event, context, callback) => {
 
-  return res.data.AccessToken
-}
+  // !!!IMPORTANT: Set this flag to false, otherwise the lambda function
+  // won't quit until all DB connections are closed, which is not good
+  // if you want to freeze and reuse these connections
+  context.callbackWaitsForEmptyEventLoop = false
 
-module.exports.cageSchedule = async (event, context) => {
-
-  //let useSb = _get(event, 'pathParameters.useSandbox') || false;
-  let res = await cageSchedule()
-
-  if (!res.error) {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({...res})
-    };
-  } else {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({...res})
-    };
+  if (event.source === 'serverless-plugin-warmup') {
+    return 'Lambda is warm!'
   }
 
-};
+  // Run the request
+  app.run(event,context,callback)
+
+} // end router handler
